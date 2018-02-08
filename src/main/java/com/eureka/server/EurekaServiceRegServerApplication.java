1b5dd9b1ae5aae17d434a8df32281b84cb9ac90f
thilsenjav <thilsenjav@gmail.com>
2018-02-08 12:29:39 +0530

    First commit

---
+++ src/main/java/com/eureka/server/EurekaServiceRegServerApplication.java
@@ -0,0 +1,12 @@
+package com.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceRegServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceRegServerApplication.class, args);
+	}
+}
